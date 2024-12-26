b136da80035849c81b536d59b88856518c5d7260
chigoziem <chigoziemi@hotmail.com>
2024-12-26 06:30:43 +0100

    hero

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
@@ -13,12 +14,17 @@ const Header = () => {
   const toogleNavigation = () => {
     if (openNavigation) {
       setOpenNavigation(false);
+      enablePageScroll()
     } else {
         setOpenNavigation(true);
+        disablePageScroll()
     }
 }
 
     const handleClick = () => {
+        if(!openNavigation) return;
+
+        enablePageScroll();
         setOpenNavigation(false);
     }
 
